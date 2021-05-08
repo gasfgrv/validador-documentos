b1436d283126106092997755fe6765444b10d4af
Gustavo Almeida <gustavo.almeida@autbank.com.br>
2021-05-08 15:09:48 -0300

    correções

--- src/gusto/validador/cpf/ValidaCPF.java
+++ src/gusto/validador/cpf/ValidaCPF.java
@@ -7,36 +7,24 @@ import static java.lang.Integer.parseInt;
 
 public class ValidaCPF {
 
-	public boolean isCpfValido(String cpf) {
-		List<String> numerosCpf = new ArrayList<>();
+    public boolean isCpfValido(String cpf) {
+        var calculoValidacao = 0;
+        var digitoValidador1 = 0;
+        var digitoValidador2 = 0;
 
-		int calculoValidacao = 0;
+        List<String> numerosCpf = new ArrayList<>();
 
-		int digitoValidador1 = 0;
-		int digitoValidador2 = 0;
+        for (char num : cpf.toCharArray()) if (num != '.' && num != '-') numerosCpf.add(Character.toString(num));
+        for (var i = 0; i < numerosCpf.size() - 2; i++) calculoValidacao += parseInt(numerosCpf.get(i)) * (i + 1);
+        digitoValidador1 = calculoValidacao % 11;
 
-		for (char num : cpf.toCharArray()) {
-			if (num != '.' && num != '-') {
-				numerosCpf.add(Character.toString(num));
-			}
-		}
+        calculoValidacao = 0;
 
-		for (int i = 0; i < numerosCpf.size() - 2; i++) {
-			calculoValidacao += parseInt(numerosCpf.get(i)) * (i + 1);
-		}
+        for (var i = 0; i < (numerosCpf.size() - 1); i++) calculoValidacao += parseInt(numerosCpf.get(i)) * i;
+        digitoValidador2 = calculoValidacao % 11;
 
-		digitoValidador1 = calculoValidacao % 11;
-		
-		calculoValidacao = 0;
-
-		for (int i = 0; i < numerosCpf.size() - 1; i++) {
-			calculoValidacao += parseInt(numerosCpf.get(i)) * i;
-		}
-
-		digitoValidador2 = calculoValidacao % 11;
-
-		return numerosCpf.get(numerosCpf.size() - 2).equals(Integer.toString(digitoValidador1))
-				&& numerosCpf.get(numerosCpf.size() - 1).equals(Integer.toString(digitoValidador2));
-	}
+        return numerosCpf.get(numerosCpf.size() - 2).equals(Integer.toString(digitoValidador1))
+                && numerosCpf.get(numerosCpf.size() - 1).equals(Integer.toString(digitoValidador2));
+    }
 
 }
--- src/gusto/validador/rg/ValidaRg.java
+++ src/gusto/validador/rg/ValidaRg.java
@@ -4,29 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidaRg {
-	public boolean isRgValido(String rg) {
-		// Serve apenas para RGs de São Paulo
-		List<String> numerosRg = new ArrayList<>();
+    public boolean isRgValido(String rg) {
+        // Serve apenas para RGs de São Paulo
+        List<String> numerosRg = new ArrayList<>();
+        var digitoValidador = 0;
 
-		int digitoValidador = 0;
+        for (char num : rg.toCharArray()) if (num != '.' && num != '-') numerosRg.add(Character.toString(num));
+        for (var i = 0; i < numerosRg.size() - 1; i++) digitoValidador += Integer.parseInt(numerosRg.get(i)) * (i + 2);
+        digitoValidador = 11 - (digitoValidador % 11);
 
-		for (char num : rg.toCharArray()) {
-			if (num != '.' && num != '-') {
-				numerosRg.add(Character.toString(num));
-			}
-		}
+        boolean isValidoComDigitoNumerico = numerosRg.get(numerosRg.size() - 1).equals(Integer.toString(digitoValidador))
+                || numerosRg.get(numerosRg.size() - 1).equals("11") && digitoValidador == 11;
 
-		for (int i = 0; i < numerosRg.size() - 1; i++) {
-			digitoValidador += Integer.parseInt(numerosRg.get(i)) * (i + 2);
-		}
+        boolean isValidoComDigitoX = numerosRg.get(numerosRg.size() - 1).equals("X") && digitoValidador == 10;
 
-		digitoValidador = 11 - (digitoValidador % 11);
-
-		boolean isValidoComDigitoNumerico = numerosRg.get(numerosRg.size() - 1).equals(Integer.toString(digitoValidador))
-				|| numerosRg.get(numerosRg.size() - 1).equals("11") && digitoValidador == 11;
-
-		boolean isValidoComDigitoX = numerosRg.get(numerosRg.size() - 1).equals("X") && digitoValidador == 10;
-
-		return isValidoComDigitoNumerico || isValidoComDigitoX;
-	}
+        return isValidoComDigitoNumerico || isValidoComDigitoX;
+    }
 }
